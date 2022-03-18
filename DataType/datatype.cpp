e4af581294b69ad3558d79859459908532d5faf8
Offliners <eric21904@gmail.com>
2022-03-18 16:05:46 +0800

    add datatype

---
+++ DataType/datatype.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<limits>
+
+using namespace std;
+
+int main(void)
+{
+    int int_max = numeric_limits<int>::max();
+    int int_min = numeric_limits<int>::min();
+    bool bool_max = numeric_limits<bool>::max();
+    bool bool_min = numeric_limits<bool>::min();
+    float float_max = numeric_limits<float>::max();
+    float float_min = numeric_limits<float>::min();
+    double double_max = numeric_limits<double>::max();
+    double double_min = numeric_limits<double>::min();    
+    long long longlong_max = numeric_limits<long long>::max();
+    long long longlong_min = numeric_limits<long long>::min();	
+    char char_max = numeric_limits<char>::max();
+    char char_min = numeric_limits<char>::min();
+
+    cout << "============================" << endl;
+    cout << "int is " << sizeof(int) << " bytes." << endl;
+    cout << "bool is " << sizeof(bool) << " bytes." << endl;
+    cout << "float is " << sizeof(float) << " bytes." << endl;
+    cout << "double is " << sizeof(double) << " bytes." << endl;
+    cout << "long long is " << sizeof(long long) << " bytes." << endl;
+    cout << "char is " << sizeof(char) << " bytes." << endl;
+    cout << endl;
+
+    cout << "============================" << endl;
+    cout << "Maximum values:" << endl;
+    cout << "int: " << int_max << endl;
+    cout << "bool: " << bool_max << endl;
+    cout << "float: " << float_max << endl;
+    cout << "double: " << double_max << endl;
+    cout << "long long: " << longlong_max << endl;
+    cout << "char: " << int(char_max) << endl;
+    cout << endl;
+
+    cout << "============================" << endl;
+    cout << "Minimum values:" << endl;
+    cout << "int: " << int_min << endl;
+    cout << "bool: " << bool_min << endl;
+    cout << "float: " << float_min << endl;
+    cout << "double: " << double_min << endl;
+    cout << "long long: " << longlong_min << endl;
+    cout << "char: " << int(char_min) << endl;
+    cout << endl;
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ OUT_DIR = build
 
 all:
 		@mkdir -p $(OUT_DIR)
-		g++ -std=c++11 src/hello.cpp -o bin/hello
+		g++ -std=c++11 Hello/hello.cpp -o $(OUT_DIR)/hello
+		g++ -std=c++11 DataType/datatype.cpp -o $(OUT_DIR)/datatype
 
 clean:
 		@rm -rf $(OUT_DIR)
