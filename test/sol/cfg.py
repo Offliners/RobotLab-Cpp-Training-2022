a08f8b1fce3655a71423de540adbcfe9b661fd7f
Offliners <eric21904@gmail.com>
2022-06-29 16:35:36 +0800

    update limit

--- test/sol/cfg.py
+++ test/sol/cfg.py
@@ -1,6 +1,6 @@
 cfg = {
     'N' : 50,
-    'time_limit' : 5,         # ms
-    'memory_limit' : 10,      # KB
+    'time_limit' : 1000,         # ms
+    'memory_limit' : 1024,      # KB
     'questions' : ['q1', 'q2', 'q4']
 }
